e5af15dc25a445b36e0a56376ea555c8ed7e1b61
Jena Pratik (FCA) <t1553pj@cag.cdroot.net>
2025-03-26 08:52:22 +0530

    custom action json

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,15 @@ const github= require('@actions/github');
 const exec=require('@actions/exec');
 
 function run() {
+    // get input values
     core.notice('Hello from my custome js actions!');
+    const bucket = core.getInput('bucket',{required:true});
+    const region = core.getInput('region',{required:true});
+    const distfolder = core.getInput('dist-folder',{required:true});
+
+    // upload file
+    const s3Uri = `s3://${bucket}`;
+    exec.exec('aws s3 sync ${distfolder} ${s3Uri} --region ${region}');
 }
 
 run();
