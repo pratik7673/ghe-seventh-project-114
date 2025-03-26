7af02b45daf1e5e1eb84f38af0b248592a8c6a92
Jena Pratik (FCA) <t1553pj@cag.cdroot.net>
2025-03-26 08:56:40 +0530

    fixed code

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,17 +1,23 @@
-const core= require('@actions/core');
-const github= require('@actions/github');
-const exec=require('@actions/exec');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
 
-function run() {
-    // get input values
-    core.notice('Hello from my custome js actions!');
-    const bucket = core.getInput('bucket',{required:true});
-    const region = core.getInput('region',{required:true});
-    const distfolder = core.getInput('dist-folder',{required:true});
+async function run() {
+    try {
+        // Get input values
+        core.notice('Hello from my custom JS action!');
+        const bucket = core.getInput('bucket', { required: true });
+        const region = core.getInput('region', { required: true });
+        const distfolder = core.getInput('dist-folder', { required: true });
 
-    // upload file
-    const s3Uri = `s3://${bucket}`;
-    exec.exec('aws s3 sync ${distfolder} ${s3Uri} --region ${region}');
+        // Upload files to S3
+        const s3Uri = `s3://${bucket}`;
+        await exec.exec(`aws s3 sync ${distfolder} ${s3Uri} --region ${region}`);
+        
+        core.notice(`Files successfully uploaded to ${s3Uri}`);
+    } catch (error) {
+        core.setFailed(`Action failed: ${error.message}`);
+    }
 }
 
 run();
